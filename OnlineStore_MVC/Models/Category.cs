2c14a3566f4ec82ffeeb10852df04c9334d29b66
Ilya Kukhtikov <ikukhtikov@outlook.com>
2024-02-05 04:58:00 +0300

    Add Update and Delete operations

--- OnlineStore_MVC/Models/Category.cs
+++ OnlineStore_MVC/Models/Category.cs
@@ -7,8 +7,11 @@ namespace OnlineStore_MVC.Models
     {
         [Key]
         public int CategoryId { get; set; }
+        [Required]
         public string? CategoryName { get; set; }
         [DisplayName("Display Order")]
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage ="Порядок отображения категорий должен быть больше 0.")]
         public int DisplayOrder { get; set; }
 
     }
