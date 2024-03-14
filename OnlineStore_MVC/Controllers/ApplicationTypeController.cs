555deae87e1b7aec37d9787b5be47e24abcfd519
Кухтиков Илья Александрович <kukhtikov_ilya@outlook.com>
2024-03-14 04:17:36 +0300

    1. Add Edit and Delete operations for ApplicationType 2. Add font Awesome and icons

--- OnlineStore_MVC/Controllers/ApplicationTypeController.cs
+++ OnlineStore_MVC/Controllers/ApplicationTypeController.cs
@@ -34,5 +34,65 @@ namespace OnlineStore_MVC.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        // Метод GET для операции EDIT (редактирование ApplicationType)
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // Метод POST для операции EDIT (обнавление строки в БД)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ApplicationType.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // Метод GET для операции DELETE (удаление категории)
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        // Метод POST для операции DELETE (удаление строки в БД)
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.ApplicationType.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
