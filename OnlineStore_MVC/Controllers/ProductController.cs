7296d2c0cb869242120b3d1e533f8b88907de4f5
Кухтиков Илья Александрович <kukhtikov_ilya@outlook.com>
2024-03-15 02:19:44 +0300

    1. Add Upsert method in Product Controller 2. Remove Update method in Product Controller

--- OnlineStore_MVC/Controllers/ProductController.cs
+++ OnlineStore_MVC/Controllers/ProductController.cs
@@ -25,49 +25,35 @@ namespace OnlineStore_MVC.Controllers
             return View(objList);
         }
 
-        // Метод GET для операции CREATE (создание категории)
-        public IActionResult Create() 
+        // Метод GET - UPSERT
+        public IActionResult Upsert(int? id) 
         {
+            Product product = new Product();
+            if (id == null)
+            {
+                // this is for create
+                return View(product);
+            }
+            else
+            {
+                product = _db.Product.Find(id);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                return View(product);
+            }
             return View();
         }
 
-        // Метод POST для операции CREATE (добавление в БД)
+        // Метод POST для операции UPSERT (добавление в БД)
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Category obj)
+        public IActionResult Upsert(Category obj)
         {
             if(ModelState.IsValid)
             {
                 _db.Category.Add(obj);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(obj);
-        }
-
-        // Метод GET для операции EDIT (редактирование категории)
-        public IActionResult Edit(int? id)
-        {
-            if(id == null || id == 0)
-            {
-                return NotFound();
-            }
-            var obj = _db.Category.Find(id);
-            if(obj == null)
-            {
-                return NotFound();
-            }
-            return View(obj);
-        }
-
-        // Метод POST для операции EDIT (обнавление строки в БД)
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Category obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Category.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
