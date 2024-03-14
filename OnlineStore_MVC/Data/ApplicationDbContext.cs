25b595175272ad9a9b2ca25e84501c4fd6e96a7f
Кухтиков Илья Александрович <kukhtikov_ilya@outlook.com>
2024-03-14 06:00:22 +0300

    1. Add Product Model

--- OnlineStore_MVC/Data/ApplicationDbContext.cs
+++ OnlineStore_MVC/Data/ApplicationDbContext.cs
@@ -12,5 +12,6 @@ namespace OnlineStore_MVC.Data
 
         public DbSet<Category> Category { get; set; }
         public DbSet<ApplicationType> ApplicationType { get; set; }
+        public DbSet<Product> Product { get; set; }
     }
 }
---
+++ OnlineStore_MVC/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineStore_MVC.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        [Range(1,int.MaxValue)]
+        public double Price { get; set; }
+        public string? ImageURL { get; set; }
+        [Display(Name = "Category type")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
