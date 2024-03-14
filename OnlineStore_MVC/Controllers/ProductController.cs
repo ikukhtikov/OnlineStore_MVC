a46c8c2e3f1fa5ffd6e3a69500a367e071ec4685
Кухтиков Илья Александрович <kukhtikov_ilya@outlook.com>
2024-03-14 06:20:56 +0300

    1. Add Controller and View Product

---
+++ OnlineStore_MVC/Controllers/ProductController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore_MVC.Data;
+using OnlineStore_MVC.Models;
+
+namespace OnlineStore_MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        // Метод отображения категорий
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index() 
+        {
+            IEnumerable<Product> objList = _db.Product;
+
+            foreach(var obj in objList)
+            {
+                obj.Category = _db.Category.FirstOrDefault(u => u.CategoryId == obj.CategoryId);
+            };
+
+            return View(objList);
+        }
+
+        // Метод GET для операции CREATE (создание категории)
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        // Метод POST для операции CREATE (добавление в БД)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // Метод GET для операции EDIT (редактирование категории)
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // Метод POST для операции EDIT (обнавление строки в БД)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // Метод GET для операции DELETE (удаление категории)
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        // Метод POST для операции DELETE (удаление строки в БД)
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
