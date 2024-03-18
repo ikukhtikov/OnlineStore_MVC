c62ae4fb2c5ec83e7f468aadd57be3a6c1bfd264
Кухтиков Илья Александрович <kukhtikov_ilya@outlook.com>
2024-03-18 07:36:41 +0300

    ViewModel for Hone page

--- OnlineStore_MVC/Controllers/HomeController.cs
+++ OnlineStore_MVC/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore_MVC.Data;
 using OnlineStore_MVC.Models;
+using OnlineStore_MVC.Models.ViewModels;
 using System.Diagnostics;
 
 namespace OnlineStore_MVC.Controllers
@@ -7,15 +10,22 @@ namespace OnlineStore_MVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM()
+            {
+                Products = _db.Product.Include(u => u.Category).Include(u => u.ApplicationType),
+                Categories = _db.Category
+            };
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
---
+++ OnlineStore_MVC/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineStore_MVC.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Product>? Products { get; set; }
+        public IEnumerable<Category>? Categories { get; set; }
+    }
+}
